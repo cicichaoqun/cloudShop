1dd81a16d971f2105865bdc38fdba778ba1aba18
chenchaoqun <396217969@qq.com>
2019-01-17 10:19:54 +0800

    init

---
+++ src/pages/uiDemo/flex/index.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="page">
+        <div class="page__hd">
+            <h1 class="page__title">Flex</h1>
+            <p class="page__desc">Flex布局</p>
+        </div>
+        <div class="page__bd page__bd_spacing">
+            <div class="weui-flex">
+                <div class="weui-flex__item"><div class="placeholder">weui</div></div>
+            </div>
+            <div class="weui-flex">
+                <div class="weui-flex__item"><div class="placeholder">weui</div></div>
+                <div class="weui-flex__item"><div class="placeholder">weui</div></div>
+            </div>
+            <div class="weui-flex">
+                <div class="weui-flex__item"><div class="placeholder">weui</div></div>
+                <div class="weui-flex__item"><div class="placeholder">weui</div></div>
+                <div class="weui-flex__item"><div class="placeholder">weui</div></div>
+            </div>
+            <div class="weui-flex">
+                <div class="weui-flex__item"><div class="placeholder">weui</div></div>
+                <div class="weui-flex__item"><div class="placeholder">weui</div></div>
+                <div class="weui-flex__item"><div class="placeholder">weui</div></div>
+                <div class="weui-flex__item"><div class="placeholder">weui</div></div>
+            </div>
+            <div class="weui-flex">
+                <div><div class="placeholder">weui</div></div>
+                <div class="weui-flex__item"><div class="placeholder">weui</div></div>
+                <div><div class="placeholder">weui</div></div>
+            </div>
+        </div>
+    </div>
+</template>
+<style lang="less" scoped>
+@import url("../base.less");
+.page{
+    .placeholder {
+        margin: 5px;
+        padding: 0 10px;
+        background-color: #EBEBEB;
+        height: 2.3em;
+        line-height: 2.3em;
+        text-align: center;
+        color: #CFCFCF;
+    }
+}
+</style>
---
+++ src/pages/uiDemo/home/index.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="page">
+    <div class="page__hd">
+        ui组件库
+    </div>
+    <div class="page__bd page__bd_spacing">
+        <ul>
+            <li>
+                <!-- <div class="weui-flex js_category">
+                    <p class="weui-flex__item">ui组件库</p>
+                </div> -->
+                <div class="page__category">
+                    <div class="weui-cells page__category-content">
+                        <a class="weui-cell weui-cell_access js_item" data-id="button" href="/pages/uiDemo/button/index">
+                            <div class="weui-cell__bd">
+                                <p>Button</p>
+                            </div>
+                            <div class="weui-cell__ft"></div>
+                        </a>
+                        <a class="weui-cell weui-cell_access js_item" data-id="input" href="/pages/uiDemo/loadmore/index">
+                            <div class="weui-cell__bd">
+                                <p>Loadmore</p>
+                            </div>
+                            <div class="weui-cell__ft"></div>
+                        </a>
+                        <a class="weui-cell weui-cell_access js_item" data-id="list" href="/pages/uiDemo/list/index">
+                            <div class="weui-cell__bd">
+                                <p>List</p>
+                            </div>
+                            <div class="weui-cell__ft"></div>
+                        </a>
+                        <a class="weui-cell weui-cell_access js_item" data-id="slider" href="/pages/uiDemo/icon/index">
+                            <div class="weui-cell__bd">
+                                <p>Icon</p>
+                            </div>
+                            <div class="weui-cell__ft"></div>
+                        </a>
+                        <a class="weui-cell weui-cell_access js_item" data-id="uploader" href="/pages/uiDemo/flex/index">
+                            <div class="weui-cell__bd">
+                                <p>Flex</p>
+                            </div>
+                            <div class="weui-cell__ft"></div>
+                        </a>
+                        <a class="weui-cell weui-cell_access js_item" data-id="uploader" href="/pages/uiDemo/panel/index">
+                            <div class="weui-cell__bd">
+                                <p>Panel</p>
+                            </div>
+                            <div class="weui-cell__ft"></div>
+                        </a>
+                    </div>
+                </div>
+            </li>
+        </ul>
+    </div>
+</div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="less">
+@import url(./example.less);
+</style>
